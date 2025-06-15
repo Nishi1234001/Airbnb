849938eb91985c0f812494851c50eb96b5f9cb69
Nishi1234001 <127599464+Nishi1234001@users.noreply.github.com>
2025-06-15 18:00:21 +0530

    Update auth.controller.js

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -14,8 +14,8 @@ export const signUp = async(req, res) => {
     let token = await genToken(user._id);
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENVIRONMENT === "production",
-      sameSite: 'strict',
+      secure:true,
+      sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
     });
     return res.status(201).json(user);
@@ -38,8 +38,8 @@ let { email, password } = req.body;
     let token = await genToken(user._id);
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENVIRONMENT === "production",
-      sameSite: 'strict',
+      secure:true,
+      sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
     });
     return res.status(200).json(user);
